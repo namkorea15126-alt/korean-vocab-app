540b536d26f7fa6895a9f30ccf51d65ecc3a091d
namkorea15126-alt <namkorea15126@gmail.com>
2026-01-26 20:41:02 +0700

    Refactor service worker installation event
    Remove unnecessary installation script from service worker.

--- service-worker.js
+++ service-worker.js
@@ -1,17 +1,1 @@
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open("vocab-cache").then(cache => {
-      return cache.addAll([
-        "./",
-        "./index.html",
-        <script>
-if ('serviceWorker' in navigator) {navigator.serviceWorker.register('service-worker.js');
-}
-</script>
 
-        "./style.css",
-        "./app.js"
-      ]);
-    })
-  );
-});
