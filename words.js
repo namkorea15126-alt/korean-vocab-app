ca1c94fb3c1a2787e0b0144fe86ed03141683ce2
namkorea15126-alt <namkorea15126@gmail.com>
2026-01-26 18:45:37 +0700

    Create words.js

---
+++ words.js
@@ -0,0 +1,6 @@
+var WORDS = [
+  { id: 1, ko: "안녕하세요", vi: "Xin chào" },
+  { id: 2, ko: "감사합니다", vi: "Cảm ơn" },
+  { id: 3, ko: "사랑", vi: "Tình yêu" },
+  { id: 4, ko: "학교", vi: "Trường học" }
+];
